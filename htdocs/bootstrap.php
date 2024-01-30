f075d9cc638024369a562b80b5b95ddf2e8c94ca
giovanniprete <g.prete@cosmobile.com>
2024-01-30 10:27:42 +0100

    messo il db come variabile globale

--- htdocs/bootstrap.php
+++ htdocs/bootstrap.php
@@ -1,4 +1,4 @@
 <?php
 session_start();
 require_once("model/database.php");
-$db = new Database("localhost", "root", "toor", "SocialNetworkDB", 3306);
+$GLOBALS['db'] = new Database("localhost", "root", "toor", "SocialNetworkDB", 3306);
