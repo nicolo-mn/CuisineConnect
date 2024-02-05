c8eb713a93fe48a4f7f6792a0c810a1bf096a2c1
Nicolò Monaldini <nicolo.monaldini@studio.unibo.it>
2024-02-05 18:41:12 +0100

    ottenimento ricetta da api

--- htdocs/view/newrecipe.php
+++ htdocs/view/newrecipe.php
@@ -1,20 +1,15 @@
 <h2 class="d-none d-md-block text-secondary text-center mb-5 fw-bold">Aggiunta ricetta</h2>
 <div class="row mb-5 mx-0">
-    <form action="#" class="col-md-10 mx-auto d-flex flex-column gap-5">
+    <form action="#" id="recipe-form" class="col-md-10 mx-auto d-flex flex-column gap-5">
         <input type="text" class="form-control bg-dark border-0 rounded-3 text-white" id="nome" placeholder="Nome ricetta">
         <textarea type="text" class="form-control bg-dark border-0 rounded-3 text-white" id="bio" rows="5" placeholder="Procedimento"></textarea>
-        <fieldset id="ingredients">
-            <div class="row justify-content-start gap-3 mb-3 mx-0">
-                <div class="col-7">
-                    <input type="text" list="ingredient-1-list" id="ingredient-1" class="form-control bg-dark border-0 rounded-3 text-white ingredient" placeholder="Ingrediente">
-                    <datalist id="ingredient-1-list">
-                        <option value="Option 1">
-                        <option value="Option 2">
-                        <option value="Option 3">
-                    </datalist>
+        <fieldset id="ingredients" class="d-flex flex-column gap-5">
+            <div class="row justify-content-start gap-3 mx-0 ingredient-container">
+                <div class="col-7 p-0">
+                    <input type="text" class="form-control bg-dark border-0 rounded-3 text-white ingredient" placeholder="Ingrediente">
                 </div>
                 <div class="col-3">
-                    <input type="text" class="form-control bg-dark border-0 rounded-3 text-white" placeholder="Quantità">
+                    <input type="text" class="form-control bg-dark border-0 rounded-3 text-white quantity" placeholder="Quantità">
                 </div>
                 <div class="col-1">
                     <p class="text-secondary">g</p>
@@ -28,9 +23,18 @@
         </div>
         <div class="row justify-content-between mt-7 mx-0">
             <input type="reset" value="Cancella" class="bg-secondary rounded-pill border-0 fw-bold py-2 col-5 ms-3">
-            <input type="submit" value="Salva modifiche" class="bg-secondary rounded-pill border-0 fw-bold py-2 col-5 me-3">
+            <input type="submit" value="Add Recipe" class="bg-secondary rounded-pill border-0 fw-bold py-2 col-5 me-3">
         </div>
+        <p class="d-none text-danger text-center">Check your ingredients for mistakes!</p>
     </form>
+    <div id="loadingOverlay" class="d-none position-absolute w-100 h-100 opacity-75 top-0">
+        <div class="d-flex flex-column align-items-center justify-content-center h-100">
+            <p class="fs-3 text-secondary mb-5">We're creating your recipe...</p>
+            <div class="spinner-border text-secondary" role="status"></div>
+        </div>
+    </div>
+
+
 </div>
 
 <script src="/web/js/newrecipe.js" type="text/javascript"></script>
--- htdocs/web/js/newrecipe.js
+++ htdocs/web/js/newrecipe.js
@@ -1,51 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
-    let ingredientCounter = 1;
-
-    const ingredient_listener =  elem => {
-        elem.addEventListener("change", function(event) {
-            const elem_id = event.target.id;
-            const query = event.target.value;
-            $.ajax({
-                url: `https://api.edamam.com/auto-complete`,
-                type: 'GET',
-                data: {
-                    'q': query,
-                    'app_id': food_api_id,
-                    'app_key': food_api_key,
-                },
-                success: function(response) {
-                    // Gestisci la risposta ottenuta dall'API qui
-                    console.log(response);
-                    // const hints = response.hints;
-                    let result = "";
-                    response.forEach(element => {
-                        result +=
-                        `
-                            <option value="${element}">
-                        `;
-                    });
-                    document.getElementById(elem_id + "-list").innerHTML = result;
-                },
-                error: function(error) {
-                    console.error('Errore nella richiesta API:', error);
-                }
-            });
-        });
-    };
-
     document.getElementById("add-ingredient").addEventListener("click", function(event) {
         event.preventDefault();
-        ingredientCounter++;
         const ingredient =
         `
-        <div class="row justify-content-start gap-3 mb-3 mx-0">
-            <div class="col-7">
-                <input type="text" list="ingredient-${ingredientCounter}-list" id="ingredient-${ingredientCounter}" class="form-control bg-dark border-0 rounded-3 text-white ingredient" placeholder="Ingrediente">
-                <datalist id="ingredient-${ingredientCounter}-list">
-                </datalist>
+        <div class="row justify-content-start gap-3 mx-0 ingredient-container">
+            <div class="col-7 p-0">
+                <input type="text" class="form-control bg-dark border-0 rounded-3 text-white ingredient" placeholder="Ingrediente">
             </div>
             <div class="col-3">
-                <input type="text" class="form-control bg-dark border-0 rounded-3 text-white" placeholder="Quantità">
+                <input type="text" class="form-control bg-dark border-0 rounded-3 text-white quantity" placeholder="Quantità">
             </div>
             <div class="col-1">
                 <p class="text-secondary">g</p>
@@ -53,43 +16,76 @@ document.addEventListener("DOMContentLoaded", function() {
         </div>
         `;
         document.getElementById("ingredients").innerHTML += ingredient;
-        ingredient_listener(document.getElementById("ingredient-" + ingredientCounter));
     });
     
-    
-
     const food_api_id = '3629cdf5';
     const food_api_key = '84f704a2a8b233566ce7dd659b78f123';
     const nutrition_api_id = 'f05f76cc';
     const nutrition_api_key = 'e7ed1f6e1b40fb76a56a496515884e1a';
     
-    document.querySelectorAll(".ingredient").forEach(ingredient_listener);    
+
+
+    document.getElementById("recipe-form").addEventListener("submit", function(event) {
+        event.preventDefault();
+        let ingredients = [];
+        const nomeRicetta = document.getElementById("nome").value;
+        document.querySelectorAll(".ingredient-container").forEach( elem => {
+                const ingredient = elem.querySelector(".ingredient").value;
+                const quantity = elem.querySelector(".quantity").value;
+                ingredients.push(`${quantity}g of ${ingredient}`);
+            }
+        );
+        console.log("ingredienti: " + ingredients);
+        $.ajax({
+            url: 'https://api.edamam.com/api/nutrition-details',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({
+                "title": nomeRicetta,
+                "ingr": ingredients
+            }),
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Basic " + btoa(nutrition_api_id + ":" + nutrition_api_key)
+            },
+            success: function(response) {
+                console.log(response);
+                // Gestisci la risposta ottenuta dall'API qui
+            },
+            error: function(error) {
+                console.error('Errore nella richiesta API:', error);
+            }
+        });
+    });
     
+
     // $.ajax({
-    //     url: 'https://api.edamam.com/api/nutrition-details',
-    //     type: 'POST',
-    //     contentType: 'application/json',
-    //     data: JSON.stringify({
-    //         "title": "Nome della tua ricetta",
-    //         "ingr": [
-    //             "100g of apple",
-    //             "100g of orange",
-    //             "100g of banana",
-    //             // Aggiungi gli ingredienti della tua ricetta
-    //         ]
-    //     }),
-    //     headers: {
-    //         "Content-Type": "application/json",
-    //         "Authorization": "Basic " + btoa(nutrition_api_id + ":" + nutrition_api_key)
+    //     url: `https://api.edamam.com/api/food-database/v2/parser`,
+    //     type: 'GET',
+    //     data: {
+    //         'ingr': "apple",
+    //         'app_id': food_api_id,
+    //         'app_key': food_api_key,
     //     },
     //     success: function(response) {
-    //         console.log(response);
     //         // Gestisci la risposta ottenuta dall'API qui
+    //         console.log(response);
+    //         // const hints = response.hints;
+    //         // let result = "";
+    //         // hints.array.forEach(element => {
+    //         //     result +=
+    //         //     `
+    //         //         <option value="${element.food.label}">
+    //         //     `;
+    //         // });
+    //         // document.getElementById("ingredient-1-list").innerHTML = result;
     //     },
     //     error: function(error) {
     //         console.error('Errore nella richiesta API:', error);
     //     }
     // });
+    
+    
     
     
     
