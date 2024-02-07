8661c6b880c99c623b58e6543580fd12f6c0fef9
Nicolò Monaldini <nicolo.monaldini@studio.unibo.it>
2024-02-07 14:09:39 +0100

    accessibilità registrazione e login

--- htdocs/view/form/register-form.php
+++ htdocs/view/form/register-form.php
@@ -1,26 +1,24 @@
-<section>
-    <form id="registrationForm" action="/register" method="post" class="d-flex flex-column px-5 gap-3">
-        <label for="email" hidden>E-mail</label>
-        <input type="email" name="email" id="email" placeholder="email"
-               class="border-0 text-white bg-dark rounded-pill py-2 px-3">
-        <span id="emailError" class="text-danger fs-7" hidden></span>
-        <label for="username" hidden>Username</label>
-        <input type="text" name="username" id="username" placeholder="Username"
-               class="border-0 text-white bg-dark rounded-pill py-2 px-3">
-        <span id="usernameError" class="text-danger fs-7" hidden></span>
-        <label for="password" hidden>Password</label>
-        <input type="password" name="password" id="password"
-               placeholder="Password"
-               class="border-0 text-white bg-dark rounded-pill py-2 px-3">
-        <span id="passwordError" class="text-danger fs-7" hidden></span>
-        <label for="password2" hidden>Password confirm</label>
-        <input type="password" name="password2" id="password2"
-               placeholder="Confirm password"
-               class="border-0 text-white bg-dark rounded-pill py-2 px-3">
-        <span id="password2Error" class="text-danger fs-7" hidden></span>
-        <input type="submit" value="Register" class="bg-secondary rounded-pill border-0 fs-4 fw-bold py-2">
-    </form>
-</section>
+<form id="registrationForm" action="/register" method="post" class="d-flex flex-column px-5 gap-3">
+    <label for="email" hidden>E-mail</label>
+    <input type="email" name="email" id="email" placeholder="email"
+           class="border-0 text-white bg-dark rounded-pill py-2 px-3">
+    <span id="emailError" class="text-danger fs-7" hidden></span>
+    <label for="username" hidden>Username</label>
+    <input type="text" name="username" id="username" placeholder="Username"
+           class="border-0 text-white bg-dark rounded-pill py-2 px-3">
+    <span id="usernameError" class="text-danger fs-7" hidden></span>
+    <label for="password" hidden>Password</label>
+    <input type="password" name="password" id="password"
+           placeholder="Password"
+           class="border-0 text-white bg-dark rounded-pill py-2 px-3">
+    <span id="passwordError" class="text-danger fs-7" hidden></span>
+    <label for="password2" hidden>Password confirm</label>
+    <input type="password" name="password2" id="password2"
+           placeholder="Confirm password"
+           class="border-0 text-white bg-dark rounded-pill py-2 px-3">
+    <span id="password2Error" class="text-danger fs-7" hidden></span>
+    <input type="submit" value="Register" class="bg-secondary rounded-pill border-0 fs-4 fw-bold py-2">
+</form>
 
 <script>
     addEventListener("DOMContentLoaded", (event) => {
